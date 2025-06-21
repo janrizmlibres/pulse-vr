e1c725990f0049946619fae30be49c1cbb257b44
Janriz Libres <libres.janriz@gmail.com>
2025-06-21 20:30:31 +0800

    develop essentials section

--- src/components/Essentials.tsx
+++ src/components/Essentials.tsx
@@ -1,12 +1,46 @@
+import Card from "./shared/Card";
+import library from "../assets/images/essentials/library.png";
+import multi from "../assets/images/essentials/multi.png";
+import release from "../assets/images/essentials/release.png";
+
+const features: {
+  image: { src: string; alt: string };
+  title: string;
+  description: string;
+}[] = [
+  {
+    image: { src: library, alt: "VR game discs" },
+    title: "Massive Library",
+    description:
+      "Access over 500 VR titles, ranging from action to exploration games.",
+  },
+  {
+    image: { src: release, alt: "Glowing calendar" },
+    title: "Fresh Content",
+    description: "New games drop every month to keep your adventures exciting.",
+  },
+  {
+    image: { src: multi, alt: "Glowing gemstone" },
+    title: "Multi-Platform",
+    description: "Play on any compatible VR headset—yours or ours.",
+  },
+];
+
 const Essentials = () => {
   return (
     <section className="bg-mono-3 py-24">
       <div className="container-content">
         <p className="mb-4 subheading">PulseVR Essentials</p>
-        <h2 className="mb-16 heading">Your gateway to immersive VR gaming</h2>
+        <h2 className="mb-16 heading-secondary">
+          Your gateway to immersive VR gaming
+        </h2>
       </div>
 
-      <div className="container-content"></div>
+      <div className="container-content grid grid-cols-3 gap-x-12">
+        {features.map(({ image, ...text }) => (
+          <Card imageSrc={image.src} altText={image.alt} {...text} />
+        ))}
+      </div>
     </section>
   );
 };
--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -1,13 +1,18 @@
 interface Props {
-  image: string;
+  imageSrc: string;
   altText: string;
+  title: string;
+  description: string;
 }
 
-const Card = ({ image, altText }: Props) => {
+const Card = ({ imageSrc, altText, title, description }: Props) => {
   return (
-    <div>
-      <div>
-        <img src={image} alt={altText} />
+    <div className="relative">
+      <span className="absolute inset-0 z-0 mt-16 rounded-xl shadow-card"></span>
+      <div className="relative z-1 flex h-full flex-col px-8 pb-8">
+        <img src={imageSrc} alt={altText} className="mb-auto w-full" />
+        <h3 className="heading-tertiary">{title}</h3>
+        <p className="body-text">{description}</p>
       </div>
     </div>
   );
