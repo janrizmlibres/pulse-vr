c10d96feb6a3d25499a8f3fda24aecd2ac0baa0e
Janriz Libres <libres.janriz@gmail.com>
2025-06-22 19:04:22 +0800

    develop initial testimonials section

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,23 +1,63 @@
 import Banner from "./shared/Banner";
+import user1 from "../assets/images/testimonials/user1.jpg";
+import user2 from "../assets/images/testimonials/user2.jpg";
+import user3 from "../assets/images/testimonials/user3.jpg";
+
+interface UserProfile {
+  name: string;
+  title: string;
+  image: string;
+  quote: string;
+}
+
+const userProfiles: UserProfile[] = [
+  {
+    name: "Taylor K.",
+    title: "Casual Gamer",
+    image: user1,
+    quote:
+      "PulseVR has completely changed the way I experience gaming. The immersion is unreal!",
+  },
+  {
+    name: "Jordan T.",
+    title: "VR Enthusiast",
+    image: user2,
+    quote:
+      "I appreciate how PulseVR keeps pushing the boundaries of virtual reality. Every session feels like a new adventure!",
+  },
+  {
+    name: "Alex J.",
+    title: "Hardcore Gamer",
+    image: user3,
+    quote:
+      "I love the variety of games available. There's always something new to try!",
+  },
+];
 
 const Testimonials = () => {
   return (
     <section className="bg-shade-3 py-12">
       <Banner
         heading="Hear from our VR community"
-        subheading="Gamer Feedback"
+        subheading="Testimonials"
         description="Hear from passionate gamers about how PulseVR’s immersive experiences and community events transformed their VR gaming journey."
       />
 
-      <div className="container-content grid grid-cols-3">
-        <img src="" alt="" />
-        <blockquote>
-          <p>
-            "PulseVR has completely changed the way I experience gaming. The
-            immersion is unreal!"
-          </p>
-          <p>— Alex, VR Enthusiast</p>
-        </blockquote>
+      <div className="container-content grid grid-cols-3 gap-6">
+        {userProfiles.map(({ name, title, image, quote }) => (
+          <div key={name} className="rounded-xl bg-shade-4 p-8">
+            <img
+              src={image}
+              alt={`${name} Profile`}
+              className="mb-4 w-3/10 rounded-full"
+            />
+            <blockquote>
+              <p className="mb-2">{name}</p>
+              <p className="mb-4 body-text">{title}</p>
+              <p>{quote}</p>
+            </blockquote>
+          </div>
+        ))}
       </div>
     </section>
   );
