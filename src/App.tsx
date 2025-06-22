4ccedb8d4b585799a5e0fdc9b653abd3b2241cf2
Janriz Libres <libres.janriz@gmail.com>
2025-06-22 10:03:22 +0800

    develop how to get started section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Essentials from "./components/Essentials";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import How from "./components/How";
 import Rentals from "./components/Rentals";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Hero />
         <Essentials />
         <Rentals />
+        <How />
       </main>
     </>
   );
--- src/components/How.tsx
+++ src/components/How.tsx
@@ -18,24 +18,37 @@ const steps = [
 
 const How = () => {
   return (
-    <section>
-      <div>
-        <p></p>
-        <h2></h2>
-        <p></p>
-      </div>
+    <section className="bg-mono-3 py-24">
+      <div className="container-content grid grid-cols-2 gap-x-12">
+        <div>
+          <p className="mb-4 subheading">Getting Started with PulseVR</p>
+          <h2 className="mb-8 heading-secondary">
+            Jump into VR in three easy steps
+          </h2>
+          <p className="body-text">
+            Ready to play? Sign up now and get your first week free!
+          </p>
+        </div>
 
-      <ol>
-        {steps.map(({ number, title, description }) => (
-          <li key={number}>
-            <div>{number}</div>
-            <div>
-              <p>{title}</p>
-              <p>{description}</p>
-            </div>
-          </li>
-        ))}
-      </ol>
+        <ol className="flex flex-col gap-6">
+          {steps.map(({ number, title, description }) => (
+            <li
+              key={number}
+              className="flex cursor-default items-center gap-8 rounded-xl border-2 border-solid border-shade-3 p-8 transition-all duration-300 hover:shadow-step-item"
+            >
+              <div className="flex h-16 w-16 items-center justify-center rounded-xl bg-mono-1 font-orbitron text-2xl font-semibold text-mono-3">
+                {number}
+              </div>
+              <div>
+                <p className="mb-3 text-xl font-semibold text-mono-1">
+                  {title}
+                </p>
+                <p className="body-text">{description}</p>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
     </section>
   );
 };
